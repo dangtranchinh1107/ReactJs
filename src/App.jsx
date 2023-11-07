6ce9aa0f86e7e40ebd20293c91d567290f53773b
dangchinh9707 <chinhdtph29556@fpt.edu.vn>
2023-11-07 23:37:06 +0700

    date picker

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
 
   return (
-    <div className='text-xl text-center bg-green-500 text-gray-200'>Hello World</div>
+    <div className='text-xl text-center bg-green-500 text-gray-200'>Date Picker</div>
   )
 }
 
